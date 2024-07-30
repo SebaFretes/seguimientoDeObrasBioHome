60d630c6444005841dfd927248aaa4250fa888fd
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-30 16:02:41 -0400

    return of ObrasList component done

--- src/components/ObrasList.jsx
+++ src/components/ObrasList.jsx
@@ -11,11 +11,14 @@ export const ObrasList = () => {
       {obras.length ? (
         <>
           <h2 className="text-3xl font-black text-center">Listado de Obras</h2>
-          <p className="text-center text-3xl mt-5 mb-10">Administrá tus <span className="font-bold text-indigo-500">Obras y Clientes</span></p>
+          <p className="text-center text-lg mt-5 mb-10">Administrá tus <span className="font-bold text-indigo-500">Obras y Clientes</span></p>
           {obras.map(item => (
-            <div key={item.id}>
-              {item.name}
-              {item.description}
+            <div key={item.id} className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+              <p className="font-bold mb-3 uppercase text-gray-700">Obra: <span className="font-normal font-case">{item.name}</span></p>
+              <p className="font-bold mb-3 uppercase text-gray-700">Cliente: <span className="font-normal font-case">{item.client}</span></p>
+              <p className="font-bold mb-3 uppercase text-gray-700">Email: <span className="font-normal font-case">{item.email}</span></p>
+              <p className="font-bold mb-3 uppercase text-gray-700">Fecha de inicio: <span className="font-normal font-case">{item.date}</span></p>
+              <p className="font-bold mb-3 uppercase text-gray-700">Descripción: <span className="font-normal font-case">{item.description}</span></p>
             </div>
           )
           )}
